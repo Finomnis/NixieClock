bd8a23bd211010b2c8988534043c59e029b34ead
Finomnis <finomnis@gmail.com>
2020-12-28 17:56:47 +0100

    Remove RealTimeClock firmware code

--- Firmware/Pins.h
+++ Firmware/Pins.h
@@ -4,20 +4,22 @@
 
 struct PINS_t
 {
-    const uint8_t REAL_TIME_CLOCK_CHANGED = PIN_PA2;
-
     const uint8_t NIXIE_DIM = PIN_PA5;
     const uint8_t NIXIE_DATA = PIN_PA6;
     const uint8_t NIXIE_LATCH = PIN_PA7;
     const uint8_t NIXIE_CLK = PIN_PB5;
 
+    const uint8_t TIME_ZONE = PIN_PA4;
+    const uint8_t NMEA_SYNC = PIN_PC2;
+
     void initializePins() const
     {
-        pinMode(REAL_TIME_CLOCK_CHANGED, INPUT);
         pinMode(NIXIE_DIM, OUTPUT);
         pinMode(NIXIE_DATA, OUTPUT);
         pinMode(NIXIE_LATCH, OUTPUT);
         pinMode(NIXIE_CLK, OUTPUT);
+        pinMode(TIME_ZONE, INPUT);
+        pinMode(NMEA_SYNC, INPUT);
     }
 };
 
